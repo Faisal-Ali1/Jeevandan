0334ca648ad3b3f438188ee4e11fc8509f444f9b
Faisal ALi <fali77616@gmail.com>
2025-07-18 22:35:13 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const otpRouter = require('./Routes/otpRoute');
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://jeevandan-frontend.onrender.com',
+    origin: process.env.ALLOWED_ORIGIN,
     credentials: true
 }));
 
